62fb80a6a484da93a412754d97153fbf4812cd92
Sqayt <74724439+Sqayt@users.noreply.github.com>
2020-12-22 19:43:15 +0500

    Initial commit

---
+++ Исходники/TestFormOne/TestFormOne/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace TestFormOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Label();
+            this.Тест = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkRed;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btn_update);
+            this.panel2.Controls.Add(this.btn_close);
+            this.panel2.Controls.Add(this.Тест);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Font = new System.Drawing.Font("Calibri Light", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(738, 78);
+            this.panel2.TabIndex = 0;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            // 
+            // btn_update
+            // 
+            this.btn_update.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_update.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_update.FlatAppearance.BorderSize = 0;
+            this.btn_update.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGreen;
+            this.btn_update.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.btn_update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_update.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_update.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_update.Location = new System.Drawing.Point(25, 27);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(111, 35);
+            this.btn_update.TabIndex = 20;
+            this.btn_update.Text = "Обновить";
+            this.btn_update.UseVisualStyleBackColor = false;
+            this.btn_update.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.AutoSize = true;
+            this.btn_close.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_close.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_close.Location = new System.Drawing.Point(711, 0);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(24, 24);
+            this.btn_close.TabIndex = 2;
+            this.btn_close.Text = "X";
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            this.btn_close.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_close_MouseDown);
+            this.btn_close.MouseEnter += new System.EventHandler(this.btn_close_MouseEnter);
+            this.btn_close.MouseLeave += new System.EventHandler(this.btn_close_MouseLeave);
+            this.btn_close.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btn_close_MouseMove);
+            // 
+            // Тест
+            // 
+            this.Тест.AutoSize = true;
+            this.Тест.Font = new System.Drawing.Font("Calibri", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Тест.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Тест.Location = new System.Drawing.Point(160, 9);
+            this.Тест.Name = "Тест";
+            this.Тест.Size = new System.Drawing.Size(452, 53);
+            this.Тест.TabIndex = 0;
+            this.Тест.Text = "Телефоный справочник";
+            this.Тест.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Тест_MouseDown);
+            this.Тест.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Тест_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gold;
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(738, 450);
+            this.panel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(738, 372);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(569, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 19);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Made by Ivanov D.S.";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label btn_close;
+        private System.Windows.Forms.Label Тест;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Исходники/TestFormOne/TestFormOne/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;//подключаю библиотеку для работы с БД
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestFormOne
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection = null;//подключение SQL в C# к внутренной БД
+        private SqlDataAdapter adapter = null;//обновляется Бд из set 
+        private SqlCommandBuilder builder = null;//строя 5 поле на комманды (удалить, изменить добавить)
+        private DataSet set = null;//времменая таблица для ввода данных и изменния бд после(проще говоря посредник перед изменениямв таблице и изменения в GridView)
+        private bool newRowAdding = false;// для команды добавить (insert)
+
+        private void LoadDate()//загружаю данные из таблицы
+        {
+            try
+            {
+                adapter = new SqlDataAdapter("SELECT *, 'Delete' AS [Command] FROM [List of clients]", sqlConnection); // подключаю в Базе данных эту таблицу и завожу 5 поле как Delete
+
+                builder = new SqlCommandBuilder(adapter);
+
+                builder.GetInsertCommand();//Завожу комманды
+                builder.GetUpdateCommand();//Завожу комманды
+                builder.GetDeleteCommand();//Завожу комманды
+
+                set = new DataSet();
+
+                adapter.Fill(set, "List of clients");//читаю(Заполняю) таблицу
+
+                dataGridView1.DataSource = set.Tables["List of clients"];//вывожу таблицу на GridView
+
+                for(int i=0;i<dataGridView1.Rows.Count; i++)//прохожу по полям в записе и вывожу новое поле на последнюю ячейку в записи
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();//Завожу новый объект linkCell построенный на основе Поле GridView
+
+                    dataGridView1[5, i] = linkCell;//индекс поле будет 6 , и индекс(последняя) по записи
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);//вывод ошибки , если вышел из исключения
+            }
+        }
+
+        private void ReloadDate()//Тот самый посредник, который берет данные из БД , но не изменяет их а выводит
+        {
+            try
+            {
+                set.Tables["List of clients"].Clear();//очищение БД , что бы не дублировались данные
+
+                adapter.Fill(set, "List of clients");
+
+                dataGridView1.DataSource = set.Tables["List of clients"];
+
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[5, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);//вывод ошибки, научился после прошлой программы
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)//в этом методе обрабатываю вывод на GriedView информации с БД при запуске программы
+        {
+            sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Данил\source\repos\TestFormOne\TestFormOne\DataBaseClientOfPhone.mdf;Integrated Security=True");// создаю соеденения для подключения БД
+
+            sqlConnection.Open();// открываю соеденение
+           
+
+            LoadDate();//начинаю загрузку БД из таблицы
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            ReloadDate();//реализую метод
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_close_MouseLeave(object sender, EventArgs e)
+        {
+
+            btn_close.ForeColor = Color.White;
+        }
+        Point last_point;
+        private void btn_close_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - last_point.X;
+                this.Top += e.Y - last_point.Y;
+            }
+        }
+
+        private void btn_close_MouseDown(object sender, MouseEventArgs e)
+        {
+            last_point = new Point(e.X, e.Y);
+        }
+
+        private void btn_close_MouseEnter(object sender, EventArgs e)
+        {
+            btn_close.ForeColor = Color.Red;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)//обрабатываю нажатия в ячейку
+        {
+            try
+            {
+                if (e.ColumnIndex == 5)//определяю в каком поле будет команды для (удалить, обновить , добавить)
+                {
+                    string task = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();//получаю текст
+                    if (task == "Delete")//для команнды удалить
+                    {
+                        if (MessageBox.Show("Удалить эту строку?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)//вывожу сообщение, и проверка пользователя на согласие удаление записи
+                        {//и если пользователь дает комманду да , то захожу в тело условия
+                            int rowIndex = e.RowIndex;
+
+                            dataGridView1.Rows.RemoveAt(rowIndex);//
+
+                            set.Tables["List of clients"].Rows[rowIndex].Delete();//удаляю эту запись из (давайте будеи говорить посредник) посредника
+
+                            adapter.Update(set, "List of clients");//обновляет Базу данных и видет если запись удалена
+                        }
+                    }
+                    else if (task == "Insert")// для комманды добавить
+                    {
+                        int rowIndex = dataGridView1.Rows.Count - 2;//для перехода на 3 поле
+
+                        DataRow row = set.Tables["List of clients"].NewRow();//для временной табл
+
+                        row["Логин"]= dataGridView1.Rows[rowIndex].Cells["Логин"].Value;//записывание данных с grid views в новые переменые row
+                        row["Пароль"]= dataGridView1.Rows[rowIndex].Cells["Пароль"].Value;
+                        row["ФИО пользователя"] = dataGridView1.Rows[rowIndex].Cells["ФИО пользователя"].Value;
+                        row["Номер телефона"]= dataGridView1.Rows[rowIndex].Cells["Номер телефона"].Value;
+
+                        set.Tables["List of clients"].Rows.Add(row);//вносим данные в БД
+                        set.Tables["List of clients"].Rows.RemoveAt(set.Tables["List of clients"].Rows.Count - 1);//во все кроме последней, потому что последнее поле проверка 
+
+                        dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 2);
+                        dataGridView1.Rows[e.RowIndex].Cells[5].Value = "Delete";
+
+                        adapter.Update(set, "List of clients");
+
+                        newRowAdding =  false;
+
+                    }
+                    else if (task == "Update")//для комманды изменить
+                    {
+                        int r = e.RowIndex;
+
+                        set.Tables["List of clients"].Rows[r]["Логин"] = dataGridView1.Rows[r].Cells["Логин"].Value; 
+                        set.Tables["List of clients"].Rows[r]["Пароль"] = dataGridView1.Rows[r].Cells["Пароль"].Value; 
+                        set.Tables["List of clients"].Rows[r]["ФИО пользователя"] = dataGridView1.Rows[r].Cells["ФИО пользователя"].Value; 
+                        set.Tables["List of clients"].Rows[r]["Номер телефона"] = dataGridView1.Rows[r].Cells["Номер телефона"].Value;
+
+                        adapter.Update(set, "List of clients");
+                        dataGridView1.Rows[e.RowIndex].Cells[5].Value = "Delete";
+                    }
+                    ReloadDate();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)//для команды Insert Добавить
+        {
+            try
+            {
+                if( newRowAdding == false)
+                {
+                    newRowAdding = true;//для добавление новостроки
+                    int lastRow = dataGridView1.Rows.Count - 2; //что бы перекинуть в 3 поле 
+
+                   DataGridViewRow row = dataGridView1.Rows[lastRow];//экзампляр класса
+
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+
+                    dataGridView1[5, lastRow] = linkCell;// что в 5 поле
+
+                    row.Cells["Command"].Value = "Insert";//меняю строку с Удалить на добавить
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if(newRowAdding == false)
+                {
+                    int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+                    DataGridViewRow eddetingRow = dataGridView1.Rows[rowIndex];
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dataGridView1[5, rowIndex] = linkCell;
+                    eddetingRow.Cells["Command"].Value = "Update";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)//контроль Введеных данных
+        {
+            e.Control.KeyPress -= new KeyPressEventHandler(Column_KeyPress);
+            if(dataGridView1.CurrentCell.ColumnIndex == 4)//что бы не вводили strnig значения(строки) в поле номера телефона
+            {
+                TextBox textBox = e.Control as TextBox;
+
+                if(textBox != null)//важное условие
+                {
+                    textBox.KeyPress += new KeyPressEventHandler(Column_KeyPress);
+                }
+            }
+        }
+        private void Column_KeyPress(object sender,KeyPressEventArgs e)//не обращает внимание на нажатие кнопки
+        {
+            if(!char.IsControl(e.KeyChar)&& !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)//для того что бы перемещать окно верхнее
+        {
+            last_point = new Point(e.X, e.Y); 
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                {
+                   this.Left += e.X - last_point.X;
+                  this.Top += e.Y - last_point.Y;
+              }
+
+        }
+
+        private void Тест_MouseDown(object sender, MouseEventArgs e)
+        {
+            last_point = new Point(e.X, e.Y);
+        }
+
+        private void Тест_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - last_point.X;
+                this.Top += e.Y - last_point.Y;
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            last_point = new Point(e.X, e.Y);
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - last_point.X;
+                this.Top += e.Y - last_point.Y;
+            }
+        }
+    }
+}
+
+      
